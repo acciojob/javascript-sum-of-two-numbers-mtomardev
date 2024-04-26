eac87f7b0d066053a8b6b2929fcb77f882cb1dbd
mtomardev <120013813+mtomardev@users.noreply.github.com>
2024-04-26 19:16:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,3 +2,15 @@ let input1 = prompt("Enter the first number:");
 let input2 = prompt("Enter the second number:");
 
 // Write your code here and print the output using alert function
+if(!isNaN(input1) && !isNaN(input2)){
+	let sum = parseInt(input1) + parseInt(input2)
+	alert(`The sum of ${input1} and ${input2} is ${sum}.`)
+}
+
+elseif(isNaN(input1) && !isNaN(input2)){
+	alert("Invalid input. Please enter a valid number.")
+}
+
+elseif(!isNaN(input1) && isNaN(input2)){
+	alert("Invalid input. Please enter a valid number.")
+}
